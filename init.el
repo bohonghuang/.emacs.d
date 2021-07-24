79be240dd4b97a364f7357cd6331972903580213
coco24 <1281299809@qq.com>
2021-07-24 13:46:35 +0800

    Initial Commit

---
+++ init.el
@@ -0,0 +1,332 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cnfonts-personal-fontnames '(("JetBrains Mono") nil nil))
+ '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   '("334a5564116c5e68271bfeef3dfb5e53a415486f41b92a485c1b3880ec48f87e" "6dfe95ae8c6e92f4c55c15299f4c1a1c3581f1a1518a5b27cee144481d1bbcb3" "4580f2e099c7ceb0c28991e0bde8dff5bbea08489fbb4e88425336b445aa4d83" "b66299d9243586497adf29f5ea980e85a62e3d7a9830b895001d986af5cfab41" "ef657fea36392ff13679548036a3e7a4f519054e74281a348d8b4728894f37b0" "02591317120fb1d02f8eb4ad48831823a7926113fa9ecfb5a59742420de206e0" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "4133d2d6553fe5af2ce3f24b7267af475b5e839069ba0e5c80416aa28913e89a" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "9f9fc38446c384a4e909b7220d15bf0c152849ef42f5b1b97356448612c77953" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "26e07f80888647204145085c4fed78e0e6652901b62a25de2b8372d71de9c0a1" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "266ecb1511fa3513ed7992e6cd461756a895dcc5fef2d378f165fed1c894a78c" "a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "8621edcbfcf57e760b44950bb1787a444e03992cb5a32d0d9aec212ea1cd5234" "6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "a6e620c9decbea9cac46ea47541b31b3e20804a4646ca6da4cce105ee03e8d0e" "9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "a7b20039f50e839626f8d6aa96df62afebb56a5bbd1192f557cb2efb5fcfb662" "d6844d1e698d76ef048a53cefe713dbbe3af43a1362de81cdd3aefa3711eae0d" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "82e57dab3f6e6d230d4db0f57be11339830cb9ecd90af730b5c447c83a05bc6c" "5185a285365a768a30ac274bdbc4437e7fd2fbe3107a1b0f2b60e900181905e0" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "b7e460a67bcb6cac0a6aadfdc99bdf8bbfca1393da535d4e8945df0648fa95fb" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" default))
+ '(debug-on-error t)
+ '(display-line-numbers t)
+ '(display-line-numbers-type 'relative)
+ '(electric-pair-mode t)
+ '(global-display-line-numbers-mode t)
+ '(global-hl-line-mode t)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(org-attach-id-dir "~/文档/org-mode/org-attach/data/")
+ '(org-attach-use-inheritance t)
+ '(org-download-display-inline-images nil)
+ '(org-download-method 'attach)
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 1.25 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+                 ("begin" "$1" "$" "$$" "\\(" "\\[")))
+ '(org-latex-compiler "xelatex")
+ '(org-latex-custom-lang-environments '((Chinese "")))
+ '(org-latex-default-packages-alist
+   '(("AUTO" "inputenc" t
+      ("pdflatex"))
+     ("T1" "fontenc" t
+      ("pdflatex"))
+     ("" "graphicx" t nil)
+     ("" "grffile" t nil)
+     ("" "longtable" nil nil)
+     ("" "wrapfig" nil nil)
+     ("" "rotating" nil nil)
+     ("normalem" "ulem" t nil)
+     ("" "amsmath" t nil)
+     ("" "textcomp" t nil)
+     ("" "amssymb" t nil)
+     ("" "capt-of" nil nil)
+     ("" "hyperref" nil nil)
+     ("a4paper,left=2cm,right=2cm,top=2cm,bottom=2cm" "geometry" nil nil)
+     ("" "ctex" nil nil)))
+ '(org-roam-graph-link-hidden-types '("file" "attachment"))
+ '(package-selected-packages
+   '(platformio-mode ccls which-key lsp-pyright rustic cmake-mode magit org-roam company-arduino arduino-mode doom-themes jetbrains-darcula-theme lsp-metals sbt-mode flycheck company yasnippet lsp-ui lsp-mode zotxt zotero org-brain multiple-cursors dtrt-indent ob-ammonite markdown-mode org-journal org-download org-ref org-pdftools))
+ '(recentf-mode t)
+ '(tool-bar-mode nil)
+ '(winner-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "https://melpa.org/packages/")
+   t))
+
+
+
+;; Install use-package if not already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; Enable defer and ensure by default for use-package
+(setq use-package-always-defer t
+      use-package-always-ensure t)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(use-package jetbrains-darcula-theme)
+(load-theme 'jetbrains-darcula )
+
+(use-package cnfonts)
+;; 让 cnfonts 随着 Emacs 自动生效。
+(cnfonts-enable)
+;; 让 spacemacs mode-line 中的 Unicode 图标正确显示。
+;; (cnfonts-set-spacemacs-fallback-fonts)
+(setq cnfonts-use-face-font-rescale t)
+
+
+
+;; ======================================== Scala ========================================
+
+;; Enable scala-mode for highlighting, indentation and motion commands
+(use-package scala-mode
+  :interpreter
+    ("scala" . scala-mode))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+)
+
+;; Enable nice rendering of diagnostics like compile errors.
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook  (scala-mode . lsp)
+         (lsp-mode . lsp-lens-mode)
+  :config
+  ;; Uncomment following section if you would like to tune lsp-mode performance according to
+  ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+  ;;       (setq gc-cons-threshold 100000000) ;; 100mb
+  ;;       (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  ;;       (setq lsp-idle-delay 0.500)
+  ;;       (setq lsp-log-io nil)
+  ;;       (setq lsp-completion-provider :capf)
+  (setq lsp-prefer-flymake nil)
+  (add-hook 'lsp-completion-mode-hook (lambda ()
+                                        (when lsp-completion-mode
+                                          (set (make-local-variable 'company-backends)
+                                               (remq 'company-capf company-backends)))))  
+)
+
+;; Add metals backend for lsp-mode
+(use-package lsp-metals
+  :ensure t
+  :custom
+  ;; Metals claims to support range formatting by default but it supports range
+  ;; formatting of multiline strings only. You might want to disable it so that
+  ;; emacs can use indentation provided by scala-mode.
+  (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
+  :hook (scala-mode . lsp))
+
+;; Enable nice rendering of documentation on hover
+;;   Warning: on some systems this package can reduce your emacs responsiveness significally.
+;;   (See: https://emacs-lsp.github.io/lsp-mode/page/performance/)
+;;   In that case you have to not only disable this but also remove from the packages since
+;;   lsp-mode can activate it automatically.
+(use-package lsp-ui)
+
+;; lsp-mode supports snippets, but in order for them to work you need to use yasnippet
+;; If you don't want to use snippets set lsp-enable-snippet to nil in your lsp-mode settings
+;;   to avoid odd behavior with snippets and indentation
+(use-package yasnippet)
+;; Use company-capf as a completion provider.
+;;
+;; To Company-lsp users:
+;;   Company-lsp is no longer maintained and has been removed from MELPA.
+;;   Please migrate to company-capf.
+(use-package company
+  :hook (scala-mode . company-mode)
+  :config
+  (setq lsp-completion-provider :capf))
+
+;; Use the Debug Adapter Protocol for running tests and debugging
+(use-package posframe
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+  )
+(use-package dap-mode
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode)
+  )
+
+;; ================================================================================
+
+;; =======================================Rust =========================================
+(use-package rustic
+  ;; :ensure
+  ;; :bind (:map rustic-mode-map
+  ;;             ("M-j" . lsp-ui-imenu)
+  ;;             ("M-?" . lsp-find-references)
+  ;;             ("C-c C-c l" . flycheck-list-errors)
+  ;;             ("C-c C-c a" . lsp-execute-code-action)
+  ;;             ("C-c C-c r" . lsp-rename)
+  ;;             ("C-c C-c q" . lsp-workspace-restart)
+  ;;             ("C-c C-c Q" . lsp-workspace-shutdown)
+  ;;            ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  ;; (setq rustic-format-on-save t)
+  ;; (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)
+)
+
+;; (defun rk/rustic-mode-hook ()
+;;   ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+;;   ;; save rust buffers that are not file visiting. Once
+;;   ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+;;   ;; no longer be necessary.
+;;   (when buffer-file-name
+;;    (setq-local buffer-save-without-query t)))
+
+;; (use-package lsp-mode
+;;   :ensure
+;;   :commands lsp
+;;   :custom
+;;   ;; what to use when checking on-save. "check" is default, I prefer clippy
+;;   (lsp-rust-analyzer-cargo-watch-command "clippy")
+;;   (lsp-eldoc-render-all t)
+;;   (lsp-idle-delay 0.6)
+;;   (lsp-rust-analyzer-server-display-inlay-hints t)
+;;   :config
+;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+;; ================================================================================
+
+;; ====================================== Python ==========================================
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
+;; ================================================================================
+
+;; ======================================== C++ ========================================
+
+;; Clangd
+
+;; (use-package which-key)
+;; 
+;; (add-hook 'c-mode-hook 'lsp)
+;; (add-hook 'c++-mode-hook 'lsp)
+;; (add-hook 'objc-mode 'lsp)
+
+;; CCLS
+
+(use-package ccls
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+;; ================================================================================
+
+;; ======================================== PlatformIO ========================================
+
+(use-package platformio-mode)
+
+;; ================================================================================
+
+(use-package magit)
+
+(use-package yasnippet)
+;(add-hook 'prog-mode-hook #'yas-minor-mode)
+(yas-global-mode t)
+(use-package yasnippet-snippets)
+
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(setq recentf-max-menu-items 10)
+
+(use-package company)
+(global-company-mode t)
+
+(defvar company-mode/enable-yas t "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+  (if (or (not company-mode/enable-yas) (and (listp backend)    (member 'company-yasnippet backend)))
+  backend
+(append (if (consp backend) backend (list backend))
+        '(:with company-yasnippet))))
+
+(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+
+;(setq org-image-actual-width 512);/ (display-pixel-width) 5))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((ammonite . t)))
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((python . t)))
+
+
+(fset 'ob-ammonite-insert
+      (kmacro-lambda-form [?# ?+ ?B ?E ?G ?I ?N ?_ ?S ?R ?C ?  ?a ?m ?m return ?# ?+ ?E ?N ?D ?_ ?S ?R ?C ?\C-a return up] 0 "%d"))
+(fset 'ob-python-insert
+      (kmacro-lambda-form [?# ?+ ?B ?E ?G ?I ?N ?_ ?S ?R ?C ?  ?p ?y ?t ?h ?o ?n return ?# ?+ ?E ?N ?D ?_ ?S ?R ?C ?\C-a return up] 0 "%d"))
+
+(setq org-agenda-files (list "~/文档/org-mode/org-agenda/summer-vacation.org"))
+
+;(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-default-notes-file "~/文档/org-mode/org-capture/captures.org")
+
+(pdf-loader-install)
+
+(require 'org-download)
+(add-hook 'dired-mode-hook 'org-download-enable)
+(setq org-noter-default-notes-file-names '("notes.org")
+      org-noter-notes-search-path '("/home/coco24/文档/org-mode/org-noter"))
+
+(use-package org-roam
+      :ensure t
+      :custom
+      (org-roam-directory (file-truename "~/文档/org-mode/org-roam/"))
+      :bind (("C-c n l" . org-roam-buffer-toggle)
+             ("C-c n f" . org-roam-node-find)
+             ("C-c n g" . org-roam-graph)
+             ("C-c n i" . org-roam-node-insert)
+             ("C-c n c" . org-roam-capture)
+             ;; Dailies
+             ("C-c n j" . org-roam-dailies-capture-today))
+      :config
+      (org-roam-setup)
+      ;; If using org-roam-protocol
+      (require 'org-roam-protocol))
+(setq org-roam-v2-ack t)
+
+(provide 'init)
+;;;
---
+++ transient/history.el
@@ -0,0 +1,11 @@
+((magit-branch nil)
+ (magit-commit nil
+               ("--allow-empty"))
+ (magit-dispatch nil)
+ (magit-merge nil
+              ("--ff-only"))
+ (magit-pull nil)
+ (magit-push nil
+             ("--force"))
+ (magit-remote
+  ("-f")))
